fa774b683440b35d6b8d565d09010fc6220d80ac
Abhay bansal <91657975+abhaybansal5759@users.noreply.github.com>
2024-07-30 14:17:09 +0530

    first class of js

---
+++ 01_first.js
@@ -0,0 +1,18 @@
+// console.log("hello world");
+
+const accountId=1234;
+let accountEmail = "abhay@hj.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+// accountId=2; // not allowed
+
+console.log(accountId);
+console.log(accountCity);
+console.log(accountEmail);
+accountEmail="yasj@hjg.ds"
+console.log(accountEmail);
+console.log(accountPassword);
+accountPassword = "6789";
+console.log(accountPassword);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
